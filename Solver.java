49e548b2100ec81aa1823874393b2e6c3f378f5c
camwick <cwickers@pnw.edu>
2021-06-28 12:49:33 -0700

    Board string input

--- Solver.java
+++ Solver.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Solver{
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Key:\nu - unmarked cell\ne - empty cell\n# - corrosponding number\nEnter String: ");
+
+		String board = input.nextLine();
+		System.out.println(board);
 	}
 }
