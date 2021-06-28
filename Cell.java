ede3171eb905281249c627cd5ff4cadf887b819b
camwick <cwickers@pnw.edu>
2021-06-28 13:42:41 -0700

    private variables, constructor, toString

--- Cell.java
+++ Cell.java
@@ -1,3 +1,16 @@
 public class Cell{
-	
+	// private variables
+	private int id;
+	private char cellContents;
+
+	// constructors
+	public Cell(int id, char x){
+		this.id = id;
+		this.cellContents = x;
+	}
+
+	// toString
+	public String toString(){
+		return "(" + id + ", " + cellContents + ")";
+	}
 }
