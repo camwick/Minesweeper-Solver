7fe5c5adf402cc2104bee771952b346b1ea03a95
camwick <cwickers@pnw.edu>
2021-06-30 04:49:26 -0700

    build boardString using rgb screen values

--- Solver.java
+++ Solver.java
@@ -4,14 +4,37 @@ public class Solver{
 	public static void main(String[] args){
 		// variables
 		Scanner input = new Scanner(System.in);
+		ScreenInput screenInput;
 		String difficulty, boardInput;
+		int row = -1, column = -1;
 
-		// user input
-		System.out.print("Board Difficulty Key:\n0: Beginner     - 9x9\n1: Intermediate - 16x16\n2: Expert       - 16x30\nResponse: ");
+		// get user difficulty input
+		System.out.print("\nBoard Difficulty Key:\n0: Beginner     - 9x9\n1: Intermediate - 16x16\n2: Expert       - 16x30\nResponse: ");
 		difficulty = input.nextLine();
+		System.out.println("");
 
-		System.out.print("\n\nGame Input String Key:\nu - unmarked cell\nf - flag\n0 - empty cell\n# - corrosponding number\nEnter String: ");
-		boardInput = input.nextLine();
+		// getting row and column from difficulty
+		switch(difficulty){
+			case "0":
+				row = 9;
+				column = 9;
+				break;
+			case "1":
+				row = 16;
+				column = 16; 
+				break;
+			case "2":
+				row = 16;
+				column = 30;
+				break;
+			default:
+				System.out.println("Invalid difficulty entered.");
+				System.exit(0);
+		}
+		screenInput = new ScreenInput(row, column);
+
+		// getting board string
+		boardInput = screenInput.getBoardString();
 
 		// board creation
 		Board game = new Board(0, boardInput);
