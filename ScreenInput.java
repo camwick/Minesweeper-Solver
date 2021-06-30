6ee3aa224c6d9ed7ad5959b7a960c083c62f0ec0
camwick <cwickers@pnw.edu>
2021-06-30 03:41:40 -0700

    file creation - constructor

--- ScreenInput.java
+++ ScreenInput.java
@@ -1,3 +1,31 @@
+import java.awt.*;
+
 public class ScreenInput{
-	
+	// variables
+	Robot bot;
+
+	// constructors
+	public ScreenInput(){
+		try{
+			this.bot = new Robot();
+		}
+		catch(AWTException e){
+			System.out.println(e);
+		}
+	}
+
+	// public methods
+	public String getBoardString(Board board){
+
+	}
 }
+
+/*
+First Cell position: 
+	- 1086x396
+
+Green - rgb(0, 128, 0)
+red   - rgb(255, 0, 0)
+blue  - rgb(0, 0, 255)
+gray  - rgb(198, 198, 198)
+*/
