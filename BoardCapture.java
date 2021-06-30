daae5dba2e3392393e6ed7f7c2a98507e184f85d
camwick <cwickers@pnw.edu>
2021-06-30 02:08:10 -0700

    screenshot

--- BoardCapture.java
+++ BoardCapture.java
@@ -1,3 +1,33 @@
-public class BoardCapture{
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
+public class BoardCapture{
+	// private variables
+	Robot bot;
+	String savePath = "./Images/board.png";
+
+	// constructors
+	public BoardCapture(){
+		try{ 
+			this.bot = new Robot();
+		}
+		catch(AWTException e){
+			System.out.println(e);
+		}
+	}
+
+	public void screenshot(){
+		Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		BufferedImage img =  this.bot.createScreenCapture(screenRect);
+
+		try{	
+			ImageIO.write(img, "png", new File(this.savePath));
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+	}
 }
