6f2c77a5df960f3ab695333c580304c602c2ebbf
camwick <cwickers@pnw.edu>
2021-07-03 01:53:02 -0700

    add distinction between empty cells and unclicked cells

--- ScreenInput.java
+++ ScreenInput.java
@@ -9,7 +9,8 @@ public class ScreenInput{
 	private Color two = new Color(0, 128, 0);
 	private Color three = new Color(255, 0, 0);
 	private Color four = new Color(0, 0, 128);
-	private Color unmarked = new Color(198, 198, 198);
+	private Color empty = new Color(198, 198, 198);
+	private Color unmarked = new Color(128,128,128);
 
 	// constructors
 	public ScreenInput(int row, int column){
@@ -33,21 +34,20 @@ public class ScreenInput{
 			for(int j = 0; j < this.row; j++){
 				cellColor = this.bot.getPixelColor(1086 + 40 * j, 396 + 40 * i);
 
-				if(cellColor.equals(this.unmarked)){
-					output += "0";
+				if(cellColor.equals(this.empty)){
+					if(this.bot.getPixelColor(1086 + 40 * j + 17, 396 + 40 * i + 17).equals(this.unmarked))
+						output += "u";
+					else 
+						output += "0";
 				}
-				else if(cellColor.equals(this.one)){
+				else if(cellColor.equals(this.one))
 					output += "1";
-				}
-				else if(cellColor.equals(this.two)){
+				else if(cellColor.equals(this.two))
 					output += "2";
-				}
-				else if(cellColor.equals(this.three)){
+				else if(cellColor.equals(this.three))
 					output += "3";
-				}
-				else if(cellColor.equals(this.four)){
+				else if(cellColor.equals(this.four))
 					output += "4";
-				}
 				else{
 					System.out.println("Found color not saved: " + cellColor + "\nRow: " + i + "\nColumn: " + j);
 					System.exit(0);
