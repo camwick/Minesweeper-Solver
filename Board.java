91345a41820ef40154aa772ac2cbd8689762feea
camwick <cwickers@pnw.edu>
2021-11-03 13:57:30 -0500

    flip difficulty control between Solver and Board

--- Board.java
+++ Board.java
@@ -1,35 +1,13 @@
 public class Board {
-    private int width = -1;
-    private int height = -1;
+    private final int width;
+    private final int height;
+    private int mineCount;
     private Cell[][] board;
 
-    public Board(int difficulty) {
-        switch (difficulty) {
-        case 1:
-            this.width = 9;
-            this.height = 9;
-            this.board = new Cell[this.width][this.height];
-            break;
-        case 2:
-            this.width = 16;
-            this.height = 16;
-            this.board = new Cell[this.width][this.height];
-            break;
-        case 3:
-            this.width = 30;
-            this.height = 16;
-            this.board = new Cell[this.width][this.height];
-            break;
-        case 4:
-            this.width = 30;
-            this.height = 20;
-            this.board = new Cell[this.width][this.height];
-            break;
-        default:
-            System.out.println("Invalid difficulty entered: " + difficulty);
-            System.exit(1);
-            break;
-        }
+    public Board(int width, int height, int mines) {
+        this.width = width;
+        this.height = height;
+        this.mineCount = mines;
 
         // initiate Cell objects
         for (int x = 0; x < this.height; x++) {
--- Solver.java
+++ Solver.java
@@ -1,5 +1,24 @@
-public class Solver{
-    public static void main(String[] args){
-        Board board = new Board(1);
+public class Solver {
+    private Board gameBoard;
+
+    public Solver(int difficulty) {
+        switch (difficulty) {
+        case 1:
+            this.gameBoard = new Board(9, 9, 10);
+            break;
+        case 2:
+            this.gameBoard = new Board(16, 16, 40);
+            break;
+        case 3:
+            this.gameBoard = new Board(30, 16, 99);
+            break;
+        case 4:
+            this.gameBoard = new Board(30, 20, 130);
+            break;
+        default:
+            System.out.println("Invalid difficulty entered: " + difficulty);
+            System.exit(1);
+            break;
+        }
     }
 }
