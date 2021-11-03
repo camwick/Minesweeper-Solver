ad4caa34306bd3bcb8c6afb05626aab0d2186fe5
camwick <cwickers@pnw.edu>
2021-11-03 13:45:34 -0500

    reformat

--- Board.java
+++ Board.java
@@ -1,42 +1,42 @@
-public class Board{
+public class Board {
     private int width = -1;
     private int height = -1;
     private Cell[][] board;
-    
-    public Board(int difficulty){
-        switch(difficulty){
-            case 1:
-                this.width = 9;
-                this.height = 9;
-                this.board = new Cell[this.width][this.height];
-                break;
-            case 2:
-                this.width = 16;
-                this.height = 16;
-                this.board = new Cell[this.width][this.height];
-                break;
-            case 3:
-                this.width = 30;
-                this.height = 16;
-                this.board = new Cell[this.width][this.height];
-                break;
-            case 4:
-                this.width = 30;
-                this.height = 20;
-                this.board = new Cell[this.width][this.height];
-                break;
-            default:
-                System.out.println("Invalid difficulty entered: " + difficulty);
-                System.exit(1);
-                break;
+
+    public Board(int difficulty) {
+        switch (difficulty) {
+        case 1:
+            this.width = 9;
+            this.height = 9;
+            this.board = new Cell[this.width][this.height];
+            break;
+        case 2:
+            this.width = 16;
+            this.height = 16;
+            this.board = new Cell[this.width][this.height];
+            break;
+        case 3:
+            this.width = 30;
+            this.height = 16;
+            this.board = new Cell[this.width][this.height];
+            break;
+        case 4:
+            this.width = 30;
+            this.height = 20;
+            this.board = new Cell[this.width][this.height];
+            break;
+        default:
+            System.out.println("Invalid difficulty entered: " + difficulty);
+            System.exit(1);
+            break;
         }
-        
+
         // initiate Cell objects
-        for(int x = 0; x < this.height; x++){
-            for(int y = 0; y < this.width; y++){
+        for (int x = 0; x < this.height; x++) {
+            for (int y = 0; y < this.width; y++) {
                 board[x][y] = new Cell();
             }
         }
-    
+
     }
 }
