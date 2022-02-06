ff214a0c0c448372864646b4e39194a4e7ce5e9e
camwick <cwickers@pnw.edu>
2022-02-06 13:30:16 -0600

    implemented 1-2 pattern

--- Patterns.java
+++ Patterns.java
@@ -1,81 +1,201 @@
-public class Patterns{
-    //because the need for the bot, gameboard and other 
-    //data is unusable here, these methods will return true
-    //if the pattern exists, then the work will be done in Solver.java
+public class Patterns {
+    // because the need for the bot, gameboard and other
+    // data is unusable here, these methods will return true
+    // if the pattern exists, then the work will be done in Solver.java
+    private Cell mine;
+    private Cell cell;
 
-    public boolean onetwoPattern(Cell cell)
-    {
-        //if the board is
-        //  U   U   U
-        //  1   2   1
-        //then
-        //  F   U   F
-        //  1   2   1
+    public Patterns(Cell cell) {
+        this.cell = cell;
+    }
 
-        //also works vertically
-        //  E   1   U
-        //  E   2   U
-        //  E   1   U
-        //then
-        //  E   1   F
-        //  E   2   U
-        //  E   1   F
+    public boolean onetwoPattern() {
+        // if the board is
+        // U U U
+        // 1 2 1
+        // then
+        // F U F
+        // 1 2 1
 
-        
-        //also make sure to take into account the number of flags
-        //adjacent to the cell
-        //if the number is 2 but there is a flag already adjacent
-        //the 1 2 pattern will not work
+        // also works vertically
+        // E 1 U
+        // E 2 U
+        // E 1 U
+        // then
+        // E 1 F
+        // E 2 U
+        // E 1 F
 
-        Cell[] adjacent = cell.getAdjacent();
-        
-        //check horizontal first, only check for pattern if 
-        //numeric value is 2. This is done in Solver.java
+        // also make sure to take into account the number of flags
+        // adjacent to the cell
+        // if the number is 2 but there is a flag already adjacent
+        // the 1 2 pattern will not work
 
-        //assuming cellContents == 2
+        Cell[] adjacent = this.cell.getAdjacent();
 
-        //horizontal possibilities
+        // check horizontal first, only check for pattern if
+        // numeric value is 2. This is done in Solver.java
 
-        //if the 1 is on the left of 2
-        //This currently only checks for mines above the pattern - add a below check as well
-        if(Character.getNumericValue(adjacent[3].getContents()) - cell.getAdjFlags() == 1 && adjacent[2].getContents() == 'U')
-        {     
-            if(adjacent[0].getContents() == 'U' && Character.getNumericValue(adjacent[4].getContents()) - cell.getAdjFlags() == 2){
-                //flag mine
-                return true;
-            }
-        }  
-         
-        //if the 1 is on the right of 2
-        //also currently only checks for mines above the pattern - add a below check as well
-        if(Character.getNumericValue(adjacent[4].getContents()) - cell.getAdjFlags()  == 1 && adjacent[0].getContents() == 'U'){   
-            if(adjacent[2].getContents() == 'U' && Character.getNumericValue(adjacent[3].getContents()) - cell.getAdjFlags() == 2){
-                //flag mine
-                return true;    
+        // assuming cellContents == 2
+
+        // horizontal possibilities
+
+        // if the 1 is on the left of 2
+        // This currently only checks for mines above the pattern - add a below check as
+        // well
+
+        if (this.cell.getNumUnlickedAdj() > 3)
+            return false;
+
+        // 1 left of 2
+        if (Character.getNumericValue(adjacent[3].getContents()) - adjacent[3].getAdjFlags() == 1) {
+            // top left U
+            if (adjacent[0].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 1; i <= 2; ++i) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[2];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
+                // bottom left U
+            } else if (adjacent[5].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 6; i <= 7; ++i) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[7];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
             }
         }
-            
 
-        /*vertical possibilities
-        //if the 1 is above the 2
-        //This currently only checks for mines to the right of the pattern - add a left check as well
-        */
-        if(Character.getNumericValue(adjacent[1].getContents()) - cell.getAdjFlags() == 1 && adjacent[7].getContents() == 'U'){
-            if(adjacent[2].getContents() == 'U' && Character.getNumericValue(adjacent[6].getContents()) - cell.getAdjFlags() == 2){
-                //flag mine
-                return true;
+        // if the 1 is on the right of 2
+        if (Character.getNumericValue(adjacent[4].getContents()) - adjacent[4].getAdjFlags() == 1) {
+            // top left U
+            if (adjacent[0].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 1; i <= 2; ++i) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[0];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
+                // bottom left U
+            } else if (adjacent[5].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 6; i <= 7; ++i) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[5];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
             }
         }
 
-        //if the 1 is below the 2
-        //This currently only checks for mines to the right of the pattern - add a left check as well
-        if(Character.getNumericValue(adjacent[6].getContents()) - cell.getAdjFlags() == 1 && adjacent[2].getContents() == 'U'){
-            if(adjacent[7].getContents() == 'U' && Character.getNumericValue(adjacent[1].getContents()) - cell.getAdjFlags() == 2){
-                //flag mine
-                return true;
+        // if the 1 is above the 2
+        if (Character.getNumericValue(adjacent[1].getContents()) - adjacent[1].getAdjFlags() == 1) {
+            // top left U
+            if (adjacent[2].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 4; i <= 7; i += 3) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[7];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
+                // bottom left U
+            } else if (adjacent[0].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 3; i <= 5; i += 2) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[5];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
             }
         }
+
+        // if the 1 is below the 2
+        if (Character.getNumericValue(adjacent[6].getContents()) - adjacent[6].getAdjFlags() == 1) {
+            // top left U
+            if (adjacent[2].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 4; i <= 7; i += 3) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[2];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
+                // bottom left U
+            } else if (adjacent[0].getContents() == 'U') {
+                int counter = 0;
+                for (int i = 3; i <= 5; i += 2) {
+                    if (adjacent[i].getContents() == 'U')
+                        counter++;
+                }
+                if (counter == 2) {
+                    // mark the mine
+                    mine = adjacent[0];
+
+                    // flag mine
+                    return true;
+                } else
+                    return false;
+            }
+        }
+
+        // if no matches, returnf alse
         return false;
     }
-    
+
+    Cell getMine() {
+        return this.mine;
+    }
 }
